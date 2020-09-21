872b1dd550c0f78719e9f794417771cb68e31b7a
OleksiiHromovych <gromovuc@gmail.com>
2020-09-21 15:38:57 +0300

    Chapter 1. Getting started: A Quick Dip

---
+++ MyFirstApp/src/App.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val x = 3
+    val y = 1
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    println("This is line no matters what")
+
+    var z = 1
+    while (z < 5){
+        print("$z ")
+        z ++
+    }
+}
